1c7fa3beb6a8d14016d6e35b60c7d8d3757e2651
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-04-13 17:48:42 +0900

    画面クリア

--- main.cpp
+++ main.cpp
@@ -238,6 +238,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvHeap->GetCPUDescriptorHandleForHeapStart();
 		rtvHandle.ptr += bbIndex * device->GetDescriptorHandleIncrementSize(rtvHeapDesc.Type);
 		commandList->OMSetRenderTargets(1, &rtvHandle, false, nullptr);
+		
+		// 3.画面クリア			R	  G		B	A
+		FLOAT clearColor[] = { 0.1f,0.25f,0.5f,0.0f }; //青っぽい色
+		commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
 		// DirectX毎フレーム処理　ここまで
 
 	}
