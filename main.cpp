5dd289c1c33bc9b2eab88edcbcd0f7d8addf2a71
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-04-13 15:50:29 +0900

    ウィンドゥプロシージャ追加

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <Windows.h>
 
+//
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
+		//ウィンドゥが破棄された
+	case WM_DESTROY:
+		//OSに対してアプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
