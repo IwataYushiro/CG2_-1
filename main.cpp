101c89ea770deda0573c156e10191322c06d8526
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-04-13 16:45:49 +0900

    アダプタの選別

--- main.cpp
+++ main.cpp
@@ -93,6 +93,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		//動的配列に追加
 		adapters.push_back(tmpAdapter);
 	}
+	//妥当なアダプタを選別
+	for (size_t i = 0; i < adapters.size(); i++)
+	{
+		DXGI_ADAPTER_DESC3 adapterDesc;
+		//アダプタの情報を取得
+		adapters[i]->GetDesc3(&adapterDesc);
+
+		//ソフトウェアデバイスを回避
+		if (!(adapterDesc.Flags&DXGI_ADAPTER_FLAG3_SOFTWARE))
+		{
+			//デバイスを採用してループを抜ける
+			tmpAdapter = adapters[i];
+			break;
+		}
+	}
 	//ゲームループ
 	while (true)
 	{
