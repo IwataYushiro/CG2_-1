2f62102b45b087d51cb441bff20cdcc7a4cc1a5f
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-04-20 14:03:06 +0900

    ここから2-2　頂点シェーダーの説明

--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -2,3 +2,5 @@ float4 main( float4 pos : POSITION ) : SV_POSITION
 {
 	return pos;
 }
+//　return pos + float4(<-Xオフセット+>,Yオフセット,0,0) 指定しただけずれる
+//　return pos * float4(X拡長, Y拡張, Y縮小, Y縮小) 指定しただけ拡縮される
