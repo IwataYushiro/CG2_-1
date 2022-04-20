660d2eab9f73dc9c0c3b21998604b7476e9040ab
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-04-20 14:22:43 +0900

    頂点レイアウトについての補足説明

--- main.cpp
+++ main.cpp
@@ -216,9 +216,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	// 描画初期化処理　ここから
 	XMFLOAT3 vertices[] = {
-		{-0.5f,-0.5f,0.0f},//左下
-		{-0.5f,+0.5f,0.0f},//左上
-		{+0.5f,-0.5f,0.0f},//右下
+		{-0.5f,-0.5f,0.0f},//左下	Xが-で左　Yが-で下
+		{-0.5f,+0.5f,0.0f},//左上	Xが-で左　Yが+で上
+		{+0.5f,-0.5f,0.0f},//右下	Xが+で右　Yが-で下
 	};
 	//頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
 	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
@@ -325,10 +325,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//頂点レイアウト
 	D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
 		{
-			"POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,
-			D3D12_APPEND_ALIGNED_ELEMENT,
-			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0
+			"POSITION",										//セマンティック名
+			0,												//同じセマンティック名が複数あるときに使うインデックス(0でいい)
+			DXGI_FORMAT_R32G32B32_FLOAT,					//要素数とビット数を表す(XYZの3つでfloat型なのでR32G32B32_FLOAT)
+			0,												//入力スロットインデックス(0でいい)
+			D3D12_APPEND_ALIGNED_ELEMENT,					//データのオフセット値(D3D12_APPEND_ALIGNED_ELEMENTだと自動設定)
+			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,		//入力データ種別(標準はD3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA)
+			0												//一度に描画するインスタンス数(0でいい)
 		},//(1行で書いた方が見やすい)
+		//座標以外に色、テクスチャUV等を渡す場合はさらに続ける
 	};
 	//グラフィックスパイプライン設定
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc{};
