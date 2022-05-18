25e9c0660d73b20cca7cb945d4e499528b52865d
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-05-18 17:26:49 +0900

    ピクセルシェーダーで図形を塗りつぶす(4-2)

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -1,8 +1,11 @@
 #include "Basic.hlsli"
 
+Texture2D<float4> tex : register(t0);
+SamplerState smp      : register(s0);
+
 float4 main(VSOutput input) : SV_TARGET
 {
-	return float4(input.uv,0,1);
+	return float4(tex.Sample(smp,input.uv));
 }
 
 //float4(Ô,—Î,Â,ƒ¿’l(1)) ”ÍˆÍ@0.0f`1.0f
