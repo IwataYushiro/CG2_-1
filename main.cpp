408ca3a1323f43c600c8a619f54ce74ad43f5e39
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-05-19 00:44:38 +0900

    SRVのフォーマットを直す

--- main.cpp
+++ main.cpp
@@ -459,11 +459,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	D3D12_CPU_DESCRIPTOR_HANDLE srvHandle = srvHeap->GetCPUDescriptorHandleForHeapStart();
 	//シェーダーリソースビュー設定
 	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};				//設定構造体
-	srvDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;		//RGBA float 
+	srvDesc.Format = resDesc.Format; 
 	srvDesc.Shader4ComponentMapping =
 		D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
 	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;	//2Dテクスチャ
-	srvDesc.Texture2D.MipLevels = 1;
+	srvDesc.Texture2D.MipLevels = resDesc.MipLevels;
 
 	//ハンドルの指す位置にシェーダーリソースビュー作成
 	device->CreateShaderResourceView(texbuff, &srvDesc, srvHandle);
