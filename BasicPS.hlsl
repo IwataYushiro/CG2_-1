533bcde1769ca56738a6e2d071dcbc7bc6c38b69
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-05-18 15:47:35 +0900

    シェーダー用ヘッダーファイルのインクルード(4-1終了)

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -1,6 +1,8 @@
-float4 main() : SV_TARGET
+#include "Basic.hlsli"
+
+float4 main(VSOutput input) : SV_TARGET
 {
-	return color;
+	return float4(input.uv,0,1);
 }
 
 //float4(Ô,—Î,Â,ƒ¿’l(1)) ”ÍˆÍ@0.0f`1.0f
--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -1,6 +1,11 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+#include "Basic.hlsli"
+
+VSOutput main(float4 pos : POSITION, float2 uv : TEXCOORD)
 {
-	return pos;
+	VSOutput output;//ピクセルシェーダーに渡す値
+	output.svpos = pos;
+	output.uv = uv;
+	return output;
 }
 //　return pos + float4(<-Xオフセット+>,Yオフセット,0,0) 指定しただけずれる
 //　return pos * float4(X拡長, Y拡張, Y縮小, Y縮小) 指定しただけ拡縮される
