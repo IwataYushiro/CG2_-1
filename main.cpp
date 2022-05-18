9298adb8599039e59f7b7065d6977482ae1b58c8
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-05-18 13:43:51 +0900

    三角形ストリップ

--- main.cpp
+++ main.cpp
@@ -657,7 +657,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		commandList->SetGraphicsRootSignature(rootSignature);
 
 		//プリミティブ形状の設定コマンド
-		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);	//三角形リスト
+		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);	//三角形ストリップ
 		// 頂点バッファビューの設定コマンド
 		commandList->IASetVertexBuffers(0, 1, &vdView);
 		//定数バッファビュー(CBV)の設定コマンド
