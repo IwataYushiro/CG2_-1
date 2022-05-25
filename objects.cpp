ce95635eb3013250e9739cb875bd47ea4db2738f
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-05-25 14:07:07 +0900

    メンバ関数定義

--- objects.cpp
+++ objects.cpp
@@ -7,3 +7,18 @@ Objects::Objects()
 Objects::~Objects()
 {
 }
+
+void Objects::Initialize()
+{
+	
+}
+
+void Objects::Update()
+{
+
+}
+
+void Objects::Draw()
+{
+
+}
--- objects.h
+++ objects.h
@@ -1,9 +1,30 @@
 #pragma once
+#include <Windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
 class Objects
 {
 public:
 	Objects();
 	~Objects();
+	/// <summary>
+	/// ‰Šú‰»
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// –ˆƒtƒŒ[ƒ€ˆ—
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// •`‰æ
+	/// </summary>
+	void Draw();
 
 private:
 
