404bf22eaedaf42ab0684da86d05fe170789e0ce
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-05-26 14:41:47 +0900

    元に戻した

--- Mesh.cpp
+++ Mesh.cpp
@@ -12,19 +12,26 @@ Mesh::~Mesh()
 
 void Mesh::Initialize()
 {
-	XMFLOAT3 vertices[] = {
-		
+	XMFLOAT3 vertices_[][3] = {
+		{
 		{-0.5f,-0.5f,0.0f},//左下	Xが-で左　Yが-で下
 		{-0.5f,+0.5f,0.0f},//左上	Xが-で左　Yが+で上
-		{+0.5f,-0.5f,0.0f},
-		
-		//右下	Xが+で右　Yが-で下
+		{+0.5f,-0.5f,0.0f},//右下	Xが+で右　Yが-で下
+		},
+		{
+		{-0.7f,-0.5f,0.0f},//左下	Xが-で左　Yが-で下
+		{-0.6f,+0.5f,0.0f},//左上	Xが-で左　Yが+で上
+		{-0.7f,-0.5f,0.0f},//右下	Xが+で右　Yが-で下
+		},
 	};
-	sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
+	//頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
+	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices_));
 
 	//頂点バッファの設定
+	D3D12_HEAP_PROPERTIES heapProp{};		//ヒープ設定
 	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD; //GPUへの転送用
 	//リソース設定
+	D3D12_RESOURCE_DESC resDesc{};
 	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
 	resDesc.Width = sizeVB;//頂点データ全体のサイズ
 	resDesc.Height = 1;
@@ -34,6 +41,7 @@ void Mesh::Initialize()
 	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
 	
 	//頂点バッファの生成
+	ID3D12Resource* vertBuff = nullptr;
 	result = device->CreateCommittedResource(
 		&heapProp,//ヒープ設定
 		D3D12_HEAP_FLAG_NONE,
@@ -42,11 +50,11 @@ void Mesh::Initialize()
 		nullptr,
 		IID_PPV_ARGS(&vertBuff));
 	assert(SUCCEEDED(result));
-
 	//定数バッファの設定
+	D3D12_HEAP_PROPERTIES cbHeapProp{};		//ヒープ設定
 	cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD; //GPUへの転送用
 	//リソース設定
-	
+	D3D12_RESOURCE_DESC cbResourseDesc{};
 	cbResourseDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
 	cbResourseDesc.Width = (sizeof(ConstBufferDataMaterial) + 0xff) & ~0xff;//256バイトアラインメント
 	cbResourseDesc.Height = 1;
@@ -65,13 +73,16 @@ void Mesh::Initialize()
 		IID_PPV_ARGS(&constBuffMaterial));
 	assert(SUCCEEDED(result));
 
-	//定数バッファのマッピング
+
+		//定数バッファのマッピング
+	ConstBufferDataMaterial* constMapMaterial = nullptr;
 	result = constBuffMaterial->Map(0, nullptr, (void**)&constMapMaterial);//マッピング
 	assert(SUCCEEDED(result));
 	//値を書き込むと自動的に転送される
 	constMapMaterial->color = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
 
 	//GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
+	XMFLOAT3* vertMap = nullptr;
 	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
 	assert(SUCCEEDED(result));
 
@@ -86,7 +97,10 @@ void Mesh::Initialize()
 	vdView.StrideInBytes = sizeof(XMFLOAT3);
 
 #pragma region 頂点シェーダー
-	
+	ID3DBlob* vsBlob = nullptr;		//頂点シェーダーオブジェクト
+	ID3DBlob* psBlob = nullptr;		//ピクセルシェーダーオブジェクト
+	ID3DBlob* errorBlob = nullptr;	//エラーオブジェクト
+
 	//頂点シェーダーの読み込みとコンパイル
 	result = D3DCompileFromFile(
 		L"BasicVS.hlsl",								//シェーダーファイル名
@@ -167,10 +181,11 @@ void Mesh::Update()
 
 void Mesh::Draw()
 {
+	
 	// 頂点バッファビューの設定コマンド
 	commandList->IASetVertexBuffers(0, 1, &vdView);
 	//定数バッファビュー(CBV)の設定コマンド
 	commandList->SetGraphicsRootConstantBufferView(0, constBuffMaterial->GetGPUVirtualAddress());
 	//描画コマンド
-	commandList->DrawInstanced(_countof(vertices), 1, 0, 0);	//全ての頂点を使って描画
+	commandList->DrawInstanced(_countof(vertices_), 1, 0, 0);	//全ての頂点を使って描画
 }
--- Mesh.h
+++ Mesh.h
@@ -39,48 +39,14 @@ private://メンバ変数
 	ID3D12Device* device = nullptr;
 	ID3D12GraphicsCommandList* commandList = nullptr;
 	
-	XMFLOAT3 vertices[3][10] = {
-		{
-		{-0.5f,-0.5f,0.0f},//左下	Xが-で左　Yが-で下
-		{-0.5f,+0.5f,0.0f},//左上	Xが-で左　Yが+で上
-		{+0.5f,-0.5f,0.0f},
-		}
-		//右下	Xが+で右　Yが-で下
-	};
-	//頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
-	UINT sizeVB;
-
-	//頂点バッファの設定
-	D3D12_HEAP_PROPERTIES heapProp{};		//ヒープ設定
-	//リソース設定
-	D3D12_RESOURCE_DESC resDesc{};
-
-	//頂点バッファの生成
-	ID3D12Resource* vertBuff = nullptr;
-
-	//定数バッファの設定
-	D3D12_HEAP_PROPERTIES cbHeapProp{};		//ヒープ設定
-
+	XMFLOAT3 vertices_;
 	ID3D12Resource* constBuffMaterial = nullptr;
-	
-	//リソース設定
-	D3D12_RESOURCE_DESC cbResourseDesc{};
-
-	//定数バッファのマッピング
-	ConstBufferDataMaterial* constMapMaterial = nullptr;
-
-	//GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
-	XMFLOAT3* vertMap = nullptr;
-
-	//頂点レイアウト
-	D3D12_INPUT_ELEMENT_DESC* inputLayout = nullptr;
-
 	//頂点バッファビューの作成
 	D3D12_VERTEX_BUFFER_VIEW vdView{};
 
-	ID3DBlob* vsBlob = nullptr;		//頂点シェーダーオブジェクト
-	ID3DBlob* psBlob = nullptr;		//ピクセルシェーダーオブジェクト
-	ID3DBlob* errorBlob = nullptr;	//エラーオブジェクト
 
+public://アクセッサ
+	XMFLOAT3 GetVeatices() { return vertices_; }
+	void SetVertices(XMFLOAT3 vertices);
 };
 
