d3971c63c2bbf8a980d0fd736e64414063ab6254
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-07-07 15:32:25 +0900

    シェーダで法線情報を追加

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -5,8 +5,8 @@ SamplerState smp      : register(s0);
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	return float4(tex.Sample(smp,input.uv))*color;
-	
+	//return float4(tex.Sample(smp,input.uv))*color;
+	return float4(input.normal,1);//RGBをそれぞれ法線のXYZ、Aを1で出力
 }
 
 //float4(赤,緑,青,α値(1)) 範囲　0.0f～1.0f
--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -1,10 +1,11 @@
 #include "Basic.hlsli"
 
-VSOutput main(float4 pos : POSITION, float2 uv : TEXCOORD)
+VSOutput main(float4 pos : POSITION,float3 normal : NORMAL, float2 uv : TEXCOORD)
 {
 	VSOutput output;//ピクセルシェーダーに渡す値
-	output.svpos = mul(mat,pos);
-	output.uv = uv;
+	output.svpos	= mul(mat,pos);
+	output.normal = normal;
+	output.uv		= uv;
 	return output;
 }
 //　return pos + float4(<-Xオフセット+>,Yオフセット,0,0) 指定しただけずれる
