421d43a625cfa0e8708bda8dbe6c60680eb2b04e
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-07-11 10:36:14 +0900

    拡散反射光をピクセルシェーダで計算

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -6,7 +6,9 @@ SamplerState smp      : register(s0);
 float4 main(VSOutput input) : SV_TARGET
 {
 	//return float4(tex.Sample(smp,input.uv))*color;
-	return float4(input.normal,1);//RGBをそれぞれ法線のXYZ、Aを1で出力
+	float3 light = normalize(float3(1.0f,-1.0f,1.0f));		//右下奥　向きのライト
+	float brightness = dot(-light, input.normal);			//光源へのベクトルと法線ベクトルの内積
+	return float4(brightness, brightness, brightness, 1);	//輝度をRGBに代入して出力
 }
 
 //float4(赤,緑,青,α値(1)) 範囲　0.0f～1.0f
