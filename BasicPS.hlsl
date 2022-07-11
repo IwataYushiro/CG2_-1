de4443832e9983ad04ac310d6518e4aa3fb7f80c
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-07-11 10:44:40 +0900

    テクスチャの色と合成する

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -5,11 +5,13 @@ SamplerState smp      : register(s0);
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	//return float4(tex.Sample(smp,input.uv))*color;
-	float3 light = normalize(float3(1.0f,-1.0f,1.0f));		//右下奥　向きのライト
-	float diffuse = saturate(dot(-light, input.normal));	//diffuseを[0,1]の範囲にてclampする
-	float brightness = diffuse + 0.3f;						//光源へのベクトルと法線ベクトルの内積
-	return float4(brightness, brightness, brightness, 1);	//輝度をRGBに代入して出力
+	float4 texcolor = float4(tex.Sample(smp,input.uv));
+
+	float3 light = normalize(float3(1.0f,-1.0f,1.0f));				//右下奥　向きのライト
+	float diffuse = saturate(dot(-light, input.normal));			//diffuseを[0,1]の範囲にてclampする
+	float brightness = diffuse + 0.3f;								//光源へのベクトルと法線ベクトルの内積
+	
+	return float4(texcolor.rgb * brightness, texcolor.a) * color;	//輝度をRGBに代入して出力
 }
 
 //float4(赤,緑,青,α値(1)) 範囲　0.0f～1.0f
