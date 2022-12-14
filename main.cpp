0f2d49947e30fb5d4dd312138033c5b3508f8520
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-12-14 11:01:22 +0900

    キレイキレイ(5-1終了)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include "WinApp.h"
 #include "DirectXCommon.h"
-#include "Mesh.h"
 #include "Input.h"
 
 const float PI = 3.141592f;					//const floatでいい
 
 #pragma comment(lib,"d3dcompiler.lib")
-
-using namespace DirectX;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -18,8 +15,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	DirectXCommon* dxCommon = new DirectXCommon();
 
 	Input* input = new Input();
-	Mesh* mesh = new Mesh();
-
 
 #pragma endregion
 
@@ -36,8 +31,7 @@ winApp->Initialize();
 #pragma region 描画初期化処理
 	
 	input->Initialize(winApp);
-	mesh->Initialize(winApp, dxCommon);
-
+	
 #pragma endregion
 	// 描画初期化処理　ここまで
 
@@ -51,12 +45,9 @@ winApp->Initialize();
 			break;
 		}
 
-		mesh->Update(dxCommon->GetDevice());
 		// DirectX毎フレーム処理　ここから
 		//描画前処理
 		dxCommon->PreDraw();
-
-		mesh->Draw(dxCommon->GetCommandList());
 
 		//描画後処理
 		dxCommon->PostDraw();
@@ -70,7 +61,6 @@ winApp->Initialize();
 	//解放
 	delete winApp;
 	delete dxCommon;
-	delete mesh;
 	delete input;
 	//コンソールへの文字出力
 	OutputDebugStringA("Hello DilectX!!\n");
